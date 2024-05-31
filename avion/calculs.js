622d3c32a44bfc8aa616415a2b3e1f728ca7431c
OrhanMA <orhan.madi.assani@gmail.com>
2024-05-31 09:45:43 +0200

    refactor avion

--- avion/calculs.js
+++ avion/calculs.js
@@ -4,64 +4,73 @@
 // MLW = poids max à l'atterrissage
 const keroseneDensity = 810;
 
+export const avions = [
+  {
+    EOW: 183040,
+    MZFW: 246070,
+    MTOW: 396893,
+    MLW: 265350,
+    maxCarburant: 173997,
+    maxPassengers: 400,
+    consumptionPerPassenger: 3.1,
+  },
+];
 export function flyable(
-  EOW,
-  MZFW,
-  MTOW,
-  MLW,
-  maxCarburant,
-  maxPassengers,
+  avion,
   averagePassengerWeight,
-  consumptionPerPassenger,
   numberOfPassengers,
   distance
 ) {
   const totalPassengerWeight = numberOfPassengers * averagePassengerWeight;
 
-  if (numberOfPassengers > maxPassengers) {
+  if (numberOfPassengers > avion.maxPassengers) {
     // le nombre de passagers ne doit pas dépasser la capacité totale de l'avion
     console.log(
-      `Le nombre de passagers (${numberOfPassengers}) dépasser le nombre total autorisé (${maxPassengers})`
+      `Le nombre de passagers (${numberOfPassengers}) dépasser le nombre total autorisé (${avion.maxPassengers})`
     );
     return false;
   }
 
-  if (EOW >= MZFW || EOW >= MTOW || EOW >= MLW) {
+  if (
+    avion.EOW >= avion.MZFW ||
+    avion.EOW >= avion.MTOW ||
+    avion.EOW >= avion.MLW
+  ) {
     // Si le poids à vide est plus grands que les autres types de poids, retourne faux, l'avion ne peut pas voler
     console.log(
-      `Le poids à vide (${EOW}) dépasser l'un des paramètres suivants: MZFW:${MZFW}, MTOW:${MTOW}, MLW:${MLW}`
+      `Le poids à vide (${avion.EOW}) dépasser l'un des paramètres suivants: MZFW:${avion.MZFW}, MTOW:${avion.MTOW}, MLW:${avion.MLW}`
     );
     return false;
   }
 
-  const loadedWeight = EOW + totalPassengerWeight;
-  if (loadedWeight > MZFW || loadedWeight > MLW) {
+  const loadedWeight = avion.EOW + totalPassengerWeight;
+  if (loadedWeight > avion.MZFW || loadedWeight > avion.MLW) {
     // si le poids chargé donc avion vide + passagers et supérieur, retourn faux
     console.log(
-      `Le poids chargé (${loadedWeight}) dépasse l'un des paramètres suivants: le poids max sans carburant (${MZFW}) ou le poids max à l'atterissage ${MLW}`
+      `Le poids chargé (${loadedWeight}) dépasse l'un des paramètres suivants: le poids max sans carburant (${avion.MZFW}) ou le poids max à l'atterissage ${avion.MLW}`
     );
     return false;
   }
 
   // Calculer volume de carburant nécessaire pour transporter les passagers sur la distance donnée
   const fuelNeededLiters =
-    numberOfPassengers * consumptionPerPassenger * (distance / 100);
+    numberOfPassengers * avion.consumptionPerPassenger * (distance / 100);
   // Convertir les litres en kilogrammes
   const fuelWeight = fuelNeededLiters * (keroseneDensity / 1000);
 
   // Vérifier que le poids du carburant nécessaire est inférieur à la capacité maximale de carburant
-  if (fuelWeight > maxCarburant) {
+  if (fuelWeight > avion.maxCarburant) {
     console.log(
-      `Le poids total du carburant (${fuelWeight}) dépasse le carburant maximum autorisé (${maxCarburant})`
+      `Le poids total du carburant (${fuelWeight}) dépasse le carburant maximum autorisé (${avion.maxCarburant})`
     );
     return false;
   }
 
   // Vérifier que le poids total (poids à vide + passagers + carburant) est inférieur au poids max de décollage
   const totalWeight = loadedWeight + fuelWeight;
-  if (totalWeight > MTOW) {
+  if (totalWeight > avion.MTOW) {
     console.log(
-      `Le poids total calculé dépasse le poids total au décollage maximum autorisé ${MTOW}`
+      `Le poids total calculé dépasse le poids total au décollage maximum autorisé ${avion.MTOW}`
     );
     return false;
   }
--- test/avion/test_calculs.js
+++ test/avion/test_calculs.js
@@ -1,71 +1,45 @@
-import { assert, expect } from "chai";
-import { flyable } from "../../avion/calculs.js";
+import { expect } from "chai";
+import { avions, flyable } from "../../avion/calculs.js";
 describe("Calculs avion", function () {
-  it("Flyable test correctement la capacité de vol d'un avion", function () {
-    // Les données sont prises pour le boeing des consignes
-    const EOW = 183040;
-    const MZFW = 246070;
-    const MTOW = 396893;
-    const MLW = 265350;
-    const maxCarburant = 173997;
-    const maxPassengers = 400;
-    const averagePassengerWeight = 100;
-    const consumptionPerPassenger = 3.1;
-    const numberOfPassengers = 350;
-    const distance = 10000; // en km
+  const avion = avions[0];
+  const averagePassengerWeight = 100;
+  const numberOfPassengers = 350;
+  const distance = 10000;
 
+  it("Flyable test correctement la capacité de vol d'un avion", function () {
     const result = flyable(
-      EOW,
-      MZFW,
-      MTOW,
-      MLW,
-      maxCarburant,
-      maxPassengers,
+      avion,
       averagePassengerWeight,
-      consumptionPerPassenger,
       numberOfPassengers,
       distance
     );
-
+    expect(result).to.be.true;
+  });
+  it("Test correctement le poids des passagers", () => {
+    const failingAverageWeight = flyable(
+      avion,
+      500,
+      numberOfPassengers,
+      distance
+    );
+    expect(failingAverageWeight).to.be.false;
+  });
+  it("Test correctement la distance", () => {
     const failingDistance = flyable(
-      EOW,
-      MZFW,
-      MTOW,
-      MLW,
-      maxCarburant,
-      maxPassengers,
+      avion,
       averagePassengerWeight,
-      consumptionPerPassenger,
       numberOfPassengers,
       50000
     );
+    expect(failingDistance).to.be.false;
+  });
+  it("Test correctement le nombre de passagers", () => {
     const failingPassengers = flyable(
-      EOW,
-      MZFW,
-      MTOW,
-      MLW,
-      maxCarburant,
-      maxPassengers,
+      avion,
       averagePassengerWeight,
-      consumptionPerPassenger,
       800,
       distance
     );
-    const failingAverageWeight = flyable(
-      EOW,
-      MZFW,
-      MTOW,
-      MLW,
-      maxCarburant,
-      maxPassengers,
-      500,
-      consumptionPerPassenger,
-      numberOfPassengers,
-      distance
-    );
-
-    expect(result).to.be.true;
-    expect(failingDistance).to.be.false;
     expect(failingPassengers).to.be.false;
   });
 });
